0fa45aa7231d149332406c065550c00dced829e4
BerkanYesilyurt <berkan38212@gmail.com>
2022-06-26 16:52:46 +0300

    Create Quiz

---
+++ database/migrations/2022_06_25_164017_create_quiz_questions.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('quiz_questions', function (Blueprint $table) {
+            $table->id();
+            $table->longText('question');
+            $table->string('A');
+            $table->string('B');
+            $table->string('C');
+            $table->string('D');
+            $table->string('trueOption');
+            $table->string('clue');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('quiz_questions');
+    }
+};
